2a67c80ae1e8f7f9ff36e1feaa415b146e36ae0d
Vsevolod Podtyapurin <sevakuska@outlook.com>
2022-10-07 22:25:36 +0500

    Third commit.

--- two_point.py
+++ two_point.py
@@ -1,18 +1,23 @@
-nums = [-5, -2, 4, 7, 8, 12, 16, 17, 32, 44, 48]
-
-
+'''Файл, в котором реализуется алгоритм двух указателей'''
 def two_point(iterable, k):
-    l = 0
-    r = len(nums) -1
-    while l < r:
-        if nums[l] + nums[r] == k:
-            return [nums[l], nums[r]]
-        elif nums[l] + nums[r] > k:
-            r -= 1
+    '''Алгоритм двух указателей'''
+    left_index = 0
+    right_index = len(iterable) -1
+    while left_index != right_index:
+        if iterable[left_index] + iterable[right_index] == k:
+            return [iterable[left_index], iterable[right_index]]
+        elif iterable[left_index] + iterable[right_index] > k:
+            right_index -= 1
         else:
-            l += 1
+            left_index += 1
     return []
 
 
-if __name__ == '__main__':
+def main():
+    '''Входная точка'''
+    nums = [-5, -2, 4, 7, 8, 12, 16, 17, 32, 44, 48]
     print(two_point(nums, 24))
+
+
+if __name__ == '__main__':
+    main()
